05bf7d3d8cae0f9bb347925293347c39f4fbe030
dailydevtips <info@daily-dev-tips.com>
2024-02-01 15:42:47 +0100

    link in header

--- components/layout.js
+++ components/layout.js
@@ -22,7 +22,7 @@ export default function Layout({ children }) {
   return (
     <div className='min-h-screen flex flex-col'>
       <header className='bg-purple-200 sticky top-0 h-14 flex justify-center items-center font-semibold uppercase'>
-        Next.js sidebar menu
+        <a href="https://daily-dev-tips.com/posts/creating-a-sidebar-layout-in-nextjs-with-tailwind/">Next.js sidebar menu</a>
       </header>
       <div className='flex flex-col md:flex-row flex-1'>
         <aside className='bg-fuchsia-100 w-full md:w-60'>
